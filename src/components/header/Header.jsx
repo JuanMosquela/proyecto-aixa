22e19567d7ddee8e6b001f3c19e1cdc151a97fe4
Juan Mosquella <jmosquella11@gmail.com>
2022-07-28 00:45:07 -0300

    agregados navBar y grid de imagenes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import './header.scss'
+import {MdOutlineKeyboardArrowDown} from 'react-icons/md'
 
 const Header = () => {
   return (
     <header className="header-container">
         <nav>
             <ul>
-                <li className="navbar-list">Servicios</li>
+                <li className="navbar-list">Servicios <MdOutlineKeyboardArrowDown className="arrow" /></li>
                 <Link to='/'><li className="logo">Aixa Franzoni</li></Link>
                 <li className="navbar-list">Contacto</li>
             </ul>
