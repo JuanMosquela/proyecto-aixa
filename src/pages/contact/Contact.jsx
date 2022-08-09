fe46e7ea720e11afa8a1930ff4cc7d69e3fe32a6
Juan Mosquella <jmosquella11@gmail.com>
2022-08-09 01:24:18 -0300

    incorporada validaciones de formik e integrado con emailjs

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,60 @@
 import "./contact.scss";
+import { useFormik } from "formik";
+import * as yup from 'yup';
+import emailjs, {send} from 'emailjs-com';
+import { useState } from "react";
 
 const Contact = () => {
+
+  const [loading, setLoading] = useState(false)
+
+  const [form, setForm] = useState(false)
+
+  const basicSchemas = yup.object().shape({
+   
+    firstName: yup.string().min(2, 'Mensaje muy corto!').max(50, 'Mensaje muy largo!').required('Este campo es requerido'),
+    lastName: yup.string().min(2, 'Mensaje muy corto!').max(50, 'Mensaje muy largo!').required('Este campo es requerido'),        
+    email: yup.string().email('Debes ingresar un email valido').required('Este campo es requerido'),
+    phone: yup.number().required('Este campo es requerido'),
+    message: yup.string().min(2, 'Mensaje muy corto!').max(100, 'Mensaje muy largo!').required('Este campo es requerido')
+  })
+
+  const SendEmail = (object) => {
+    
+    setLoading(true)
+    
+    emailjs.send("service_ijebytp", "template_sr0vrs4", object,"uOFt-AsEDNOS6pJPk" )    
+      .then((result) => {
+        setTimeout(() => {
+            
+          setLoading(false)
+          resetForm()
+          setForm(true)
+                    
+            
+        }, 3000);
+      }, (error) => {
+            console.log(error.text)
+  })
+ }
+
+  const { values, errors, touched,  handleSubmit, handleChange, handleBlur,  resetForm} = useFormik({
+    initialValues:{
+      firstName: '',
+      lastName:'',
+      email:'',
+      phone:'',
+      message:''
+    },
+    onSubmit: (values) => {
+      setLoading(true)
+      SendEmail(values)     
+       
+    },  
+    validationSchema: basicSchemas
+  })
+
+
   return (
     <div className="contact">
       <div className="contactBanner">
@@ -19,7 +73,7 @@ const Contact = () => {
         <span>011-5174-8813</span>
       </div>
 
-      <div className="contactForm">
+      <form className="contactForm" onSubmit={handleSubmit}>
         <h2>Formulario</h2>
 
         <div className="selectWrapper">
@@ -30,35 +84,68 @@ const Contact = () => {
 
         <div className="inputRow">
           <div className="inputWrapper">
-            <label htmlFor="nombre">Nombre</label>
-            <input type="text" name="nombre" />
+            <label htmlFor="firstName">Nombre</label>
+            <input
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}              
+              type="text" 
+              name="firstName" />
+            { touched.firstName && errors.firstName && <span className="error">{errors.firstName}</span>}
           </div>
 
           <div className="inputWrapper">
-            <label htmlFor="apellido">Apellido</label>
-            <input type="text" name="apellido" />
+            <label htmlFor="lastName">Apellido</label>
+            <input 
+              value={values.lastName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              type="text" 
+              name="lastName" />
+            { touched.lastName && errors.lastName && <span className="error">{errors.lastName}</span>}
           </div>
         </div>
 
         <div className="inputRow">
           <div className="inputWrapper">
             <label htmlFor="email">E-mail</label>
-            <input type="text" name="email" />
+            <input
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur} 
+              type="email" 
+              name="email" />
+            { touched.email && errors.email && <span className="error">{errors.email}</span>}
           </div>
 
           <div className="inputWrapper">
-            <label htmlFor="telefono">Teléfono</label>
-            <input type="text" name="telefono" />
+            <label htmlFor="phone">Teléfono</label>
+            <input 
+              value={values.phone}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              type="tel" 
+              name="phone" />
+              { touched.phone && errors.phone && <span className="error">{errors.phone}</span>}
           </div>
         </div>
 
         <div className="textareaWrapper">
-          <label htmlFor="consulta">Consulta</label>
-          <textarea name="consulta" id=""></textarea>
+          <label htmlFor="message">Consulta</label>
+          <textarea
+            value={values.message}
+            onChange={handleChange}
+            onBlur={handleBlur} 
+            name="message" 
+            id="">
+          </textarea>
+          { touched.message && errors.message && <span className="error">{errors.message}</span>}
         </div>
 
-        <button>Enviar</button>
-      </div>
+        <button type="submit">Enviar</button>
+        {form ? <span className="success">Mensaje enviado exitosamente</span> : null}
+      </form>
+      
     </div>
   );
 };
